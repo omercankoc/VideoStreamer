171e9bce1f674a4a5f1ef4ac757767515f301b14
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-12-07 17:11:50 +0300

    Added VideoStreamActivity

---
+++ Android/VideoStreamer/app/src/main/java/com/carvio/videostreamer/ActivityVideoStream.java
@@ -0,0 +1,14 @@
+package com.carvio.videostreamer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityVideoStream extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_stream);
+    }
+}
