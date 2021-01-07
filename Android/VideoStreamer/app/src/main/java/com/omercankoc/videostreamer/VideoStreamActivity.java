f42cf8a164b24ca0ab380680dbe3d302aaeda4b2
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-01-07 15:26:51 +0300

    Added App Props

---
+++ Android/VideoStreamer/app/src/main/java/com/omercankoc/videostreamer/VideoStreamActivity.java
@@ -0,0 +1,14 @@
+package com.omercankoc.videostreamer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoStreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_stream);
+    }
+}
