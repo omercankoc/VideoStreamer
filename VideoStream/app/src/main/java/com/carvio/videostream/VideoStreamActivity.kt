171df238e25270b550f0718ce0286507b7796602
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-04-17 21:03:42 +0300

    Update VideoStreamActivity.kt

--- VideoStream/app/src/main/java/com/carvio/videostream/VideoStreamActivity.kt
+++ VideoStream/app/src/main/java/com/carvio/videostream/VideoStreamActivity.kt
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import io.ktor.utils.io.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
 import java.io.IOException
 import java.net.InetSocketAddress
+import java.nio.ByteBuffer
 import java.util.*
 
 class VideoStreamActivity : AppCompatActivity() {
@@ -18,16 +17,16 @@ class VideoStreamActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_stream)
 
-        try {
-            val socket = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect(InetSocketAddress("172.24.1.1", 554))
-            val input = socket.openReadChannel()
+        GlobalScope.async {
+            try {
+                val socket = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect(InetSocketAddress("172.24.1.1", 554))
+                val input = socket.openReadChannel()
 
-            val response = input.readByte()
-            println("Server said: '$response'")
-        } catch (e : IOException){
-            e.printStackTrace()
-            println("CONNECTION LOST!")
+                val response = input.readByte()
+                println("Server said: '$response'")
+            } catch (e : IOException){
+                e.printStackTrace()
+            }
         }
-
     }
 }
