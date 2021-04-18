388cb26817a418d46137a8dc181d0b617503a491
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-04-18 23:13:07 +0300

    Update VideoStreamActivity.kt

--- VideoStream/app/src/main/java/com/carvio/videostream/VideoStreamActivity.kt
+++ VideoStream/app/src/main/java/com/carvio/videostream/VideoStreamActivity.kt
@@ -1,15 +1,18 @@
 package com.carvio.videostream
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
+import io.ktor.util.*
+import io.ktor.util.cio.*
 import io.ktor.utils.io.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.IO
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
 import java.io.IOException
-import java.net.InetSocketAddress
-import java.nio.ByteBuffer
+import java.io.InputStreamReader
+import java.net.Socket
 import java.util.*
 
 class VideoStreamActivity : AppCompatActivity() {
@@ -17,14 +20,39 @@ class VideoStreamActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_stream)
 
+        var frame = ByteArray(5000000)
+
         GlobalScope.async {
             try {
-                val socket = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect(InetSocketAddress("172.24.1.1", 554))
-                val input = socket.openReadChannel()
+                var socket : Socket = Socket("172.24.1.1", 8080)
+                var input = BufferedReader(InputStreamReader(socket.getInputStream()))
+                //val socket = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect(InetSocketAddress("172.24.1.1", 8080))
 
-                val response = input.readByte()
-                println("Server said: '$response'")
-            } catch (e : IOException){
+                while (true){
+
+                    var response = input.readLine()
+                    print(response)
+
+
+                    //val input = socket.ByteReadChannel()
+                    //val response = input.readUTF8Line()
+                    //frame += response
+                    //println("Server said: '$response'")
+                    // ---
+                    //var input = ByteArrayInputStream(frame)
+                    //var response = input.read()
+
+                    //frame += response
+
+                    //print(frame)
+
+
+                    //val input = socket.openReadChannel()
+                    //val response = input.readByte()
+                    //print("RESPONSE : $response")
+                }
+
+            } catch (e: IOException){
                 e.printStackTrace()
             }
         }
